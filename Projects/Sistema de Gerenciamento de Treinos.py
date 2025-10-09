ee050b84d2cf82e9bfd406612d38a28e719c488a
Gu5T4hv0 <vogoostah@gmail.com>
2025-10-09 15:53:16 -0300

    Treino

--- Projects/Sistema de Gerenciamento de Treinos.py
+++ Projects/Sistema de Gerenciamento de Treinos.py
@@ -1,76 +1,76 @@
 base_de_dados = [
-    "Supino reto",
-    "Supino inclinado com halteres",
-    "Crossover ou crucifixo",
-    "Remada baixa",
-    "Puxada frontal (barra ou polia)",
-    "Remada curvada",
-    "Desenvolvimento de ombros",
-    "Elevação lateral",
-    "Elevação frontal",
-    "Rosca direta",
-    "Rosca alternada com halteres",
-    "Rosca martelo",
-    "Tríceps testa",
-    "Tríceps pulley",
-    "Mergulho entre banco",
-    "Agachamento livre",
-    "Leg press",
-    "Stiff ou levantamento terra romeno",
-    "Avanços (lunges)",
-    "Elevação de panturrilha",
-    "Prancha",
-    "Abdominal infra",
-    "Abdominal oblíquo",
-    "Abdominal canivete",
-    "Supino reto",
-    "Supino inclinado com halteres",
-    "Crossover",
-    "Remada baixa",
-    "Puxada frontal (barra ou polia)",
-    "Remada curvada",
-    "Desenvolvimento de ombros",
-    "Elevação lateral",
-    "Elevação frontal",
-    "Rosca direta",
-    "Rosca alternada com halteres",
-    "Rosca martelo",
-    "Tríceps testa",
-    "Tríceps pulley",
-    "Mergulho entre bancos",
-    "Agachamento livre",
-    "Leg press",
-    "Stiff ou levantamento terra romeno",
-    "Avanços (lunges)",
-    "Elevação de panturrilha",
-    "Prancha",
-    "Abdominal infra",
-    "Abdominal oblíquo",
-    "Abdominal canivete",
-    "Supino reto",
-    "Supino inclinado com halteres",
-    "Crossover ou crucifixo",
-    "Remada baixa",
-    "Puxada frontal",
-    "Remada curvada",
-    "Desenvolvimento de ombros",
-    "Elevação lateral",
-    "Elevação frontal",
-    "Rosca direta",
-    "Rosca alternada com halter",
-    "Rosca martelo",
-    "Tríceps testa",
-    "Tríceps pulley",
-    "Mergulho entre bancos",
-    "Agachamento livre",
-    "Leg press",
-    "Stiff ou levantamento terra romeno",
-    "Avanços (lunges)",
-    "Elevação de panturrilha",
-    "Prancha",
-    "Abdominal infra",
-    "Abdominal oblíquo",
-    "Abdominal canivete"
+'Supino reto',
+'Supino inclinado com halteres',
+'Crossover ou crucifixo',
+'Crucifixo inclinado com halteres',
+'Flexão de braços',
+'Supino declinado',
+'Supino com pegada fechada',
+'Flexão diamante',
+'Peck deck (máquina voador)',
+'Flexão pliométrica',
+'Remada baixa',
+'Puxada frontal (barra ou polia)',
+'Remada curvada',
+'Puxada neutra na polia',
+'Barra fixa',
+'Remada unilateral com halter',
+'Levantamento terra',
+'Puxada atrás da nuca (barra/polia)',
+'Pullover com halteres',
+'Remada cavalinho (T-bar row)',
+'Desenvolvimento de ombros',
+'Elevação lateral',
+'Elevação frontal',
+'Encolhimento de ombros',
+'Desenvolvimento Arnold',
+'Desenvolvimento militar (barra)',
+'Face pull',
+'Crucifixo inverso (posterior)',
+'Elevação lateral inclinada',
+'Remada alta (barra ou halteres)',
+'Rosca direta',
+'Rosca alternada com halteres',
+'Rosca martelo',
+'Rosca concentrada',
+'Rosca Scott',
+'Rosca 21 (técnica)',
+'Rosca inversa (pegada pronada)',
+'Rosca spider (no banco inclinado,)'
+'Rosca em polia baixa',
+'Rosca com corda (polia)',
+'Tríceps testa',
+'Tríceps pulley',
+'Mergulho entre bancos',
+'Tríceps francês',
+'Tríceps coice com halter',
+'Tríceps na corda (polia alta)',
+'Mergulho nas paralelas',
+'Tríceps kickback unilateral',
+'Supino fechado',
+'Tríceps no banco com peso no col,o'
+'Agachamento livre',
+'Leg press',
+'Stiff ou levantamento terra rome,no'
+'Avanços (lunges)',
+'Elevação de panturrilha',
+'Cadeira extensora',
+'Cadeira flexora',
+'Agachamento sumô com halteres',
+'Afundo búlgaro',
+'Agachamento hack machine',
+'Levantamento terra sumô',
+'Passada no step (step up)',
+'Prancha',
+'Abdominal infra',
+'Abdominal oblíquo',
+'Abdominal canivete',
+'Ab wheel (roda abdominal)',
+'Elevação de pernas suspenso',
+'Prancha lateral',
+'Bicicleta no ar',
+'Crunch tradicional',
+'Russian twist (torção russa)',
 ]
 
 exercicios_emagrecer = [
@@ -198,54 +198,59 @@ dados_do_usuario  = []
 
 print('/////////////////////////////////////////////////////////////////////////////////////////// Sharp-Fit //////////////////////////////////////////////////////////////////////////////////////////')
 print('')
-print('SEJA BEM VINDO AO MELHOR APLICATIVO PARA ACADEMIA ')
+print('Seja bem-vindo ao melhor aplicativo para academia!')
 print('')
 
-genero = input('POR FAVOR NOS DIGA SEU GENERO: ')
+genero = input('Por favor, nos diga seu gênero: ')
 print('')
-nome = input('POR FAVOR NOS DIGA SEU NOME: ')
+nome = input('Por favor, nos diga seu nome: ')
 print('')
-altura = float(input('POR FAVOR NOS DIGA SUA ALTURA EM METROS:  '))
+altura = float(input('Por favor, nos diga sua altura em metros: '))
 print('')
-peso = int(input('POR FAVOR NOS DIGA SEU PESO EM  KG: '))
+peso = int(input('Por favor, nos diga seu peso em kg: '))
 print('')
-idade = int(input('POR FAVOR NOS DIGA SUA IDADE: '))
+idade = int(input('Por favor, nos diga sua idade: '))
 print('')
 
-imc = round(peso/(altura**2), 2)
+imc = round(peso / (altura ** 2), 2)
 dados_do_usuario.extend([genero, nome, imc, idade])
 
 if imc < 18.5:
-    print(f"{nome} você está na classificação: Abaixo do peso")
+    print(f"{nome}, você está na classificação: abaixo do peso")
 elif 18.5 <= imc < 24.9:
-    print(f"{nome} você está na classificação: Peso ideal")
+    print(f"{nome}, você está na classificação: peso ideal")
 else:
-    print(f"{nome} você está na classificação: Acima do peso")
-escolha= int(input("deseja um treino recomendado[2] ou criar seu propio treino[1]"))
+    print(f"{nome}, você está na classificação: acima do peso")
+
+escolha = int(input("Deseja um treino recomendado [2] ou criar seu próprio treino [1]? "))
+
 if escolha == 2:
     opcional_1 = opcional_2 = opcional_3 = 'não'
 
     if imc < 18.5:
-        opcional_1 = input('GOSTARIA DE UMA LISTA JA FEITA PARA GANHO DE MASSA MUSCULAR ? (sim/não) ')
+        opcional_1 = input('Gostaria de uma lista já feita para ganho de massa muscular? (sim/não) ')
         if opcional_1.lower() == 'sim':
             print(exercicios_ganhar_peso)
 
     if 18.5 <= imc < 24.9:
-        opcional_2 = input('GOSTARIA DE UMA LISTA DE EXERCICIO PARA DEFINIÇÃO ? (sim/não) ')
+        opcional_2 = input('Gostaria de uma lista de exercícios para definição? (sim/não) ')
         if opcional_2.lower() == 'sim':
             print(exercicios_ideal)
+        if opcional_2 == 'não':
+            print("Obrigado por usar o nosso app")
 
     if imc > 24.9:
-        opcional_3 = input('GOSTARIA DE UMA LISTA DE EXERCICIO PARA PERDA DE PESO ? (sim/não) ')
+        opcional_3 = input('Gostaria de uma lista de exercícios para perda de peso? (sim/não) ')
         if opcional_3.lower() == 'sim':
             print(exercicios_emagrecer)
 else:
    while True:
-    escolha2 = input("Escolha uma das seguintes ações \n Adicionar[1] \n Listar treino[2] \n Buscar[3] \n Fim[4]\n")
 
-    if escolha2 == "1":
+    escolha2 = input("Escolha uma das seguintes ações \n Remover [0] \n Adicionar [1] \n Listar treino [2] \n Buscar [3] \n Atualizar [4]\n Fim [5]\n ")
+
+    if escolha2 == "1": #Adicionar
         while True:
-            print("\nLista de Exercícios:")
+            print("\nLista de Exercícios: ")
             numero = 1
             for i in base_de_dados:
                 print(str(numero) + ". " + i)
@@ -263,11 +268,11 @@ else:
 
                     series = input("Quantas séries: ").strip()
                     repeticoes = input("Quantas repetições: ").strip()
-                    peso = input("Qual peso (kg): ").strip()
+                    peso = input("Qual peso (kg): ").strip() #comentário hebert: isso tem que estar num if, pq nem todo exercicio tem peso
 
                     descricao = series + " séries " + repeticoes + " repetições"
                     if peso:
-                        descricao += " " + peso + "kg"
+                        descricao += " " + peso + " kg"
 
                     exercicios_feito_pelo_usuario[exerescolhido] = descricao
                     print("\nExercicio '" + exerescolhido + "' adicionado ao treino.")
@@ -281,7 +286,7 @@ else:
                 print("Entrada invalida. Digite apenas o numero do exercício ou 'sair'.")
 
     
-    elif escolha2 == "2":
+    elif escolha2 == "2": #Listar treino
         print("\nTreino Atual ")
         if exercicios_feito_pelo_usuario:
             for nome in exercicios_feito_pelo_usuario:
@@ -289,7 +294,7 @@ else:
         else:
             print("Nenhum exericio adicionado ainda.")
 
-    if escolha2 == "3":
+    if escolha2 == "3": #Buscar
         while True:
             exercicio = input("Qual é o exercicio você deseja buscar?: ")
             if len(exercicios_feito_pelo_usuario) == 0:
@@ -306,11 +311,74 @@ else:
                     print("O exercicio mencionado não está na lista")
                     break
 
-            buscar_outro = input("Deseja buscar outro item?(sim/não): ")
+            buscar_outro = input("Deseja buscar outro item? (sim/não): ")
             if buscar_outro != "sim":
                 break
 
-    elif escolha2 == "4":
+    elif escolha2 == '0': #Remover
+        if not exercicios_feito_pelo_usuario:
+            print("Nenhum exercício adicionado ainda.")
+            continue
+
+        print("\nLista de Exercícios:")
+        chaves = list(exercicios_feito_pelo_usuario.keys())  # pega só os nomes
+        for i, nome_ex in enumerate(chaves, start=1):
+            print(f"{i}. {nome_ex} -> {exercicios_feito_pelo_usuario[nome_ex]}")
+
+        entrada = input("\nDigite o número do exercício que você quer retirar ('sair' para voltar): ").strip()
+
+        if entrada.lower() == "sair":
+            continue
+
+        if entrada.isdigit():
+            numero_escolhido = int(entrada)
+            if 1 <= numero_escolhido <= len(chaves):
+                chave = chaves[numero_escolhido - 1]
+                exercicios_feito_pelo_usuario.pop(chave)
+                print(f"\nExercício removido: {chave}\n")
+            else:
+                print("Número inválido.")
+        else:
+            print("Entrada inválida, digite um número ou 'sair'.")
+           
+    elif escolha2 == "4": #Atualizar
+        print(exercicios_feito_pelo_usuario)
+        exercicio_para_ser_modificado = input("Qual exercício deseja atualizar? ")
+        if exercicio_para_ser_modificado in exercicios_feito_pelo_usuario:
+            novo_valor_de_series = input("Digite o novo valor de séries: ")
+            novo_valor_de_repeticoes = input("Digite o novo valor de repetições: ")
+
+            if "kg" in exercicios_feito_pelo_usuario[exercicio_para_ser_modificado]: #isso verifica se tem peso no exercicio #find ta branco pq ele não sabe se oq ele vai procurar é uma string ou não
+                novo_peso = input("Digite o novo valor de peso: ")
+                if novo_valor_de_repeticoes.isdigit() and novo_valor_de_series.isdigit() and novo_peso.isdigit():
+                    exercicios_feito_pelo_usuario[exercicio_para_ser_modificado] = f"{novo_valor_de_series} séries {novo_valor_de_repeticoes} repetições {novo_peso} kg"
+                else:
+                    print("Entrada inválida, tente novamente!")
+            else:
+                if novo_valor_de_repeticoes.isdigit() and novo_valor_de_series.isdigit():
+                    exercicios_feito_pelo_usuario[exercicio_para_ser_modificado] = f"{novo_valor_de_series} séries {novo_valor_de_repeticoes} repetições"
+                else:
+                    print("Entrada inválida, tente novamente!")
+
+            continuar_atualizando = input("Deseja continuar Atualizando? (sim/não) ").strip()
+            if continuar_atualizando.lower() == "sim":
+                print()
+                print("Escolha opção 4 novamente!!")
+                print()
+                continue
+            else:
+                print()
+                print("Atualização completa, tenha um bom treino!!")
+                print()
+                print(exercicios_feito_pelo_usuario)
+                print()
+                continue
+        else:
+            print("O objeto digitado NÃO está na lista!")
+            print()
+            continue
+    elif escolha2 == "5": #Fim
+        print('Obrigado por usar o nosso app!')
         break
 
     else:
